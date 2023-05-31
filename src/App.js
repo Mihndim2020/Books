b70d8b4b8862db935a51267b606e4bd0ff89ddec
Mihndim <mihndim2016@gmail.com>
2023-05-31 21:01:57 +0100

    Merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const [books, setBooks] = useState("");
   return <div>Welcome to my App</div>;
 }
 
