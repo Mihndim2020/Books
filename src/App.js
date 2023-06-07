d7b3f7ab481d3e0f1808be86b2679e199c75563c
Mihndim <mihndim2016@gmail.com>
2023-06-07 16:09:50 +0100

    Set up local database

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.css";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
@@ -6,12 +7,17 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title: title },
-    ];
-    setBooks(updatedBooks);
+  const createBook =  async(title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    }); 
+
+    console.log(response.data);
+    // const updatedBooks = [
+    //   ...books,
+    //   { id: Math.round(Math.random() * 9999), title: title },
+    // ];
+    // setBooks(updatedBooks);
   };
 
   const editBookById = (id, newTitle) => {
